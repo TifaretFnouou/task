578864850a217307c7969b87fa354db0aa47d517
tifaret <tpnwnw@gmail.com>
2026-06-08 02:06:54 +0300

    fix: use relative paths with base: './'

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // חובה כדי שהדפדפן ימצא את הקבצים בשרת
+  base: './', // זה קריטי! זה גורם ל-index.html לחפש קבצים בתיקייה של עצמו
   server: {
     proxy: {
       '/api': {
-        target: 'https://task-4559.onrender.com', // הכתובת של האתר שלך ב-Render
+        target: 'https://task-4559.onrender.com',
         changeOrigin: true,
-        secure: true,
       }
     }
   }

67a59ce2f154088dd5f07f2156e7a09d992be742
tifaret <tpnwnw@gmail.com>
2026-06-08 01:55:59 +0300

    fix: set base url and production proxy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // חובה כדי שהדפדפן ימצא את הקבצים בשרת
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://task-4559.onrender.com', // הכתובת של האתר שלך ב-Render
         changeOrigin: true,
-        secure: false,
+        secure: true,
       }
     }
   }
